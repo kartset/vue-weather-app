a6c7efbff11ba49517d6643ca4b191499982d2bf
Kartik Setia <kartset10@gmail.com>
2023-10-23 18:14:11 +0530

    route transistions

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,27 @@ import SiteNavigation from './components/SiteNavigation.vue'
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary" >
     <SiteNavigation />
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <Transition name="page" mode="out-in" >
+        <component :is="Component" />
+      </Transition>
+    </RouterView>
+
+
   </div>
 </template>
+
+<style>
+
+.page-enter-active, 
+.page-leave-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from, 
+.page-leave-to {
+  opacity: 0;
+}
+
+</style>
 
